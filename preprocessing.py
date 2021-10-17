1a6e16da52bdab77561e5c2da135e504f923d16b
ch-epfl-287707 <ch-epfl-287707@noto.epfl.ch>
2021-10-17 21:34:40 +0200

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -16,45 +16,82 @@ def offset(x) :
 
 
 #Mettre 0 pour ceux qui sont missing values car pas significatif
+#CODE FAIT, MAIS RESTE A DISCUTER SI VRAIMENT BONNE MANIERE DE PROCEDER OU NON....... !!!!
 def missing_values(tx_train, tx_test, threshold=0.9) :
     """
     Count the missing values of each feature and delete columns (features) above a certain threshold of missing values
     (by default set to 90%).
     If the percentage is below the threshold, missing values will be kept, but set to the mean of the column.
     Be careful : *missing value = -999*
+    However, we think that for some features it makes no sense to arbitrarly set missing values to the mean as some features 
+    are just meaningless for some samples (for instance : DER_deltaeta_jet_jet is meaningless for samples with 
+    PRI_jet_num = 0). That is why we choose to set those missing values to 0 instead of the mean.
     """
     
     new_train = tx_train
     new_test = tx_test
     
+    #List of the meaningless features for samples with PRI_jet_num = 0
+    meaningless0 = [4, 5, 6, 8, 9, 12, 23, 24, 25, 26, 27, 28]
+    #List of the meaningless features for samples with PRI_jet_num = 1
+    meaningless1 = [4, 5, 6, 12, 26, 27, 28]
+    
     for col in range(tx_train.shape[1]) :
         
-        # We calculate what percentage of missing values there is in each column :
-        miss_vals = np.sum(tx_train[:,col]==-999)/999
-        percentage = miss_vals/tx_train[:,0].shape
-        
-        # We will change every missing value in the column to the mean of the column (arbitrary choice) :
         feature = new_train[:, col]
-        where = np.where(feature!=-999, True, False)
-        mean = np.mean(feature, where=where)
-        feature[feature==-999] = mean
-        new_train[:,col] = feature
         
-        # If the percentage of missing values in a column is above a treshold, we will just completely remove the column, 
-        # because the column is considered useless for the prediction
-        if(percentage >= threshold) :
-            new_train = np.delete(new_train, col, axis=1)
-            new_test = np.delete(new_test, col, axis=1)
+        if (col in meaningless0) :
+            feature[(feature==-999) & (new_train[:, 22]==0.)] = 0
+            new_train[:,col] = feature
+        if (col in meaningless1) :
+            feature[(feature==-999) & (new_train[:, 22]==1.)] = 0
+            new_train[:,col] = feature
+            
+        else :
+            # We calculate what percentage of missing values there is in each column :
+            miss_vals = np.sum(tx_train[:,col]==-999)/999
+            percentage = miss_vals/tx_train[:,0].shape
+
+            # We will change every missing value in the column to the mean of the column (arbitrary choice) :
+            where = np.where(feature!=-999, True, False)
+            mean = np.mean(feature, where=where)
+            feature[feature==-999] = mean
+            new_train[:,col] = feature
+
+            #If the percentage of missing values in a column is above a treshold, we will just completely remove the column, 
+            #because the column is considered useless for the prediction
+            if(percentage >= threshold) :
+                new_train = np.delete(new_train, col, axis=1)
+                new_test = np.delete(new_test, col, axis=1)
             
     print(tx_train.shape[1]-new_train.shape[1], ' features have been removed')
             
     return new_train, new_test
 
 
-def remove_useless_features() :
-    #useless features à la main : 15, 16, 18, 20
+def remove_useless_features(x_train, x_test) :
+    '''
+    This function removes the useless features of the parameter x :
+        - We first remove the features 15, 16, 18 and 20, because we found out those features were useless to explain y
+        according to the visualization plots in the section 'Features' distributions analysis'
+        - Then we remove the features with a standard deviation of 0, because it means that the feature is useless to
+        explain y
+    '''
+    
+    #useless features found "by hand" using the vizualisation
+    useless = [15, 16, 18, 20]
+    new_x_train = np.delete(x_train, useless, axis=1)
+    new_x_test = np.delete(x_test, useless, axis=1)
+    
+    print(x_train.shape[1]-new_x_train.shape[1], ' useless features have been removed')
     #std = 0
-    return 0
+    std_x = np.std(new_x_train, axis=0)
+    idx_std_0 = [i for i, std in enumerate(std_x) if std==0]
+    new_x_train_ = np.delete(new_x_train, idx_std_0, axis=1)
+    new_x_test_ = np.delete(new_x_test, idx_std_0, axis=1)
+    print(new_x_train.shape[1]-new_x_train_.shape[1], ' features with a standard deviation equal to 0 have been removed')
+
+    return new_x_train_ , new_x_test_
 
 
 def standardize(x, mean_x=None, std_x=None):
@@ -76,8 +113,7 @@ def standardize(x, mean_x=None, std_x=None):
     return x_std, mean_x, std_x
 
 
-#???? Do we need it ???? ÇA PEUT ÊTRE BIEN D'ENLEVER 1 DES 2 FEATURES SI IL Y EN A 2 QUI SONT CORRÉLÉES À 99% !
-#En vrai je suis d'accord, mais j'ai plus l'explication de pourquoi on devrait enlever les features hyper corrélées..
+
 def correlation(tx_train, tx_test, treshold=0.99) :
     """
     May be useful if we want to delete very correlated features
@@ -99,14 +135,14 @@ def correlation(tx_train, tx_test, treshold=0.99) :
     
     return new_train, new_test
 
-#MAIS DU COUP LÀ TU ENLÈVES LA COLONNE QUI CONTIENT UN OUTLIER ?!
-#IL FAUDRAIT PLUTÔT ENLEVER LE DIT SAMPLE PLUTÔT QUE LA FEATURE…
-def outliers(x) :
+
+def outliers(x, y) :
     """
     Delete the outliers which are aberrant values, usually due to some errors in the experiment or from the material, 
     usually not relevant and may impact the prediction.
     """
     new_x=np.array(x)
+    new_y=np.array(y)
     idx_outliers = []
     
     #Find all the outliers
@@ -119,6 +155,7 @@ def outliers(x) :
     if(len(idx_outliers)>0) :
         idx_outliers = np.array(list(set(idx_outliers)))
         new_x = np.delete(new_x, idx_outliers, axis=0)
+        new_y = np.delete(new_y, idx_outliers, axis=0)
         print('With outliers : ', x.shape)
         print('Without outliers : ', new_x.shape)
         return new_x
@@ -127,7 +164,7 @@ def outliers(x) :
         return new_x
 
 
-#    À VÉRIFIER SI LÀ ON EST EN TRAIN DE COMPARER BIEN LES OUTLIERS COLONNE PAR COLONNE COMMENTAIRE PAS CORRECT
+
 def findOutliers(x, outlierConstant=1.5):
     """
     Find the outliers using the Interquartile (IQR) method
