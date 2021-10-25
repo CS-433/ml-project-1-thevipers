007bd98c0178b942fa9c2b040a02c47df62b369e
theauv <theau.vannier@epfl.ch>
2021-10-25 15:05:06 +0200

    probleme resolu, encore des choses à voir

--- preprocessing.py
+++ preprocessing.py
@@ -15,7 +15,7 @@ def offset(x) :
     return new_x
 
 
-def missing_values(tx_train, tx_test, threshold=1) :
+def missing_values(tx_train, tx_test, threshold=0.9) :
     """
     Count the missing values of each feature and delete columns (features) above a certain threshold of missing values
     (by default set to 100%, i.e. columns containing only missing values).
@@ -25,41 +25,38 @@ def missing_values(tx_train, tx_test, threshold=1) :
     
     new_train = tx_train
     new_test = tx_test
+    #Columns we will have to delete :
+    cols = []
     
     for col in range(tx_train.shape[1]) :
         
         feature = new_train[:, col]
         
         # we calculate what percentage of missing values there is in each column :
-        miss_vals = np.sum(tx_train[:,col]==-999)/999
-        percentage = miss_vals/tx_train[:,0].shape
-
-        # we will change every missing value in the column to the mean of the column (arbitrary choice) :
-        #where = np.where(feature!=-999, True, False)
-        #mean = np.mean(feature, where=where)
-        #feature[feature==-999] = mean
+        percentage =  np.count_nonzero(feature==-999)/len(feature)
+        # we will change every missing value in the column to the meedian of the column (arbitrary choice) :
         median = np.median(feature[feature != -999])
-        new_train[:,col] = np.where(new_train[:,col]==-999, median, new_train[:,col])
+        new_train[:,col] = np.where(feature==-999, median, new_train[:,col])
         new_test[:,col] = np.where(new_test[:,col]==-999, median, new_test[:,col])
 
         # if the percentage of missing values in a column is above a treshold, we will just completely remove the column, 
         # because the column is considered useless for the prediction
         if(percentage >= threshold) :
-            new_train = np.delete(new_train, col, axis=1)
-            new_test = np.delete(new_test, col, axis=1)
+            cols.append(col)
+
+    new_train = np.delete(new_train, cols, axis=1)
+    new_test = np.delete(new_test, cols, axis=1)
 
     print(tx_train.shape[1]-new_train.shape[1], ' features have been removed')
-            
     return new_train, new_test
 
+####################
+#Remove useless features and features with 0 standard deviation :
 
 def remove_useless_features(x_train, x_test) :
     '''
-    This function removes the useless features of the parameter x :
-        - We first remove the features 15, 16, 18 and 20, because we found out those features were useless to explain y
-        according to the visualization plots in the section 'Features' distributions analysis'
-        - Then we remove the features with a standard deviation of 0, because it means that the feature is useless to
-        explain y
+    This function removes the useless features of the parameter x : We first remove the features 15, 16, 18 and 20, because we found out
+    those features were useless to explain y according to the visualization plots in the section 'Features' distributions analysis'
     '''
     
     # useless features found "by hand" using the vizualisation
@@ -68,14 +65,27 @@ def remove_useless_features(x_train, x_test) :
     new_x_test = np.delete(x_test, useless, axis=1)
     
     print(x_train.shape[1]-new_x_train.shape[1], ' useless features have been removed')
+
+    return new_x_train , new_x_test
+
+def remove_std_0(x_train, x_test) :
+    '''
+    This function removes the useless features of the parameter x : We remove the features with a standard deviation of 0, because it 
+    means that the feature is useless to explain y
+    '''
+    
+    new_x_train = x_train
+    new_x_test = x_test
+    
     # std = 0
     std_x = np.std(new_x_train, axis=0)
-    idx_std_0 = [i for i, std in enumerate(std_x) if std==0]
+    idx_std_0 = np.argwhere(std_x == 0)
     new_x_train_ = np.delete(new_x_train, idx_std_0, axis=1)
     new_x_test_ = np.delete(new_x_test, idx_std_0, axis=1)
     print(new_x_train.shape[1]-new_x_train_.shape[1], ' features with a standard deviation equal to 0 have been removed')
 
     return new_x_train_ , new_x_test_
+
 
 
 def standardize(x, mean_x=None, std_x=None):
@@ -120,7 +130,10 @@ def correlation(tx_train, tx_test, treshold=0.99) :
     return new_train, new_test
 
 
-def outliers(x, y) :
+####################
+#Outliers :
+
+def outliers(x, y, alpha=5) :
     """
     Delete the outliers which are aberrant values, usually due to some errors in the experiment or from the material, 
     usually not relevant and may impact the prediction.
@@ -131,10 +144,10 @@ def outliers(x, y) :
     
     # find all the outliers
     for i in range(new_x.shape[1]) :
-        idxs = findOutliers(x[:, i])
+        idxs = findOutliers(x[:, i], alpha=alpha)
         for j, idx in enumerate(idxs) :
             idx_outliers.append(idx)
-    
+
     # delete the lines containing outliers
     if(len(idx_outliers)>0) :
         idx_outliers = np.array(list(set(idx_outliers)))
@@ -148,15 +161,14 @@ def outliers(x, y) :
         return new_x, new_y
 
 
-
-def findOutliers(x, outlierConstant=1.5):
+def findOutliers(x, alpha, outlierConstant=1.5):
     """
     Find the outliers using the Interquartile (IQR) method
     Return the indices of the outliers
     """
     a = np.array(x)
-    upper_quartile = np.percentile(a, 75)
-    lower_quartile = np.percentile(a, 25)
+    upper_quartile = np.percentile(a, 100-alpha)
+    lower_quartile = np.percentile(a, alpha)
     IQR = (upper_quartile - lower_quartile) * outlierConstant
     quartileSet = (lower_quartile - IQR, upper_quartile + IQR)
     outliers_idx = []
@@ -165,6 +177,8 @@ def findOutliers(x, outlierConstant=1.5):
             outliers_idx.append(i)
     return outliers_idx
 
+
+#####################
 
 def build_poly(X, degree):
     """
@@ -182,33 +196,7 @@ def build_poly(X, degree):
             phi[:, 1+D*(deg-1)+i ] = np.power(X[:,i],deg)    
     return phi  
 
-def process_data(x_train, x_test, alpha=0):
-    """
-    Preprocessing: impute missing values, feature engineering, delete outliers and standardization.
-    """
-    # Missing values:
-    # consider the 0s in the 'PRI_jet_all_pt' as missing values
-    x_train[:,-1]=np.where(x_train[:,-1]==0, -999, x_train[:,-1])
-    # impute missing data
-    x_train, x_test = missing_values(x_train, x_test) 
-    
-    # Feature Engineering:
-    # Absolute value of symmetrical features
-  
-    # Other transformation for positive features
 
-    # Delete useless features
-
-    
-    # Delete outliers
-    x_train = outliers(x_train)
-    x_test = outliers(x_test)
-    
-    # Standardization
-    x_train, mean_x_train, std_x_train = standardize(x_train)
-    x_test, _, _ = standardize(x_test, mean_x_train, std_x_train)
-     
-    return x_train, x_test
 
 """
 def split_jet(train_x, train_y, tes_x, test_y) :
